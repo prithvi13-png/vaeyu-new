7bf5cf02fa6ddd503671ccb0e235a2b2a8226515
prithvi13-png <prithviprabhakarh13@gmail.com>
2026-04-15 22:56:12 +0530

    fe

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
-import { defineConfig } from "vite";
+import { defineConfig, type PluginOption } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
+import { cloudflare } from "@cloudflare/vite-plugin";
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    !process.env.VITEST ? cloudflare() : null,
+  ].filter(Boolean) as PluginOption[],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
